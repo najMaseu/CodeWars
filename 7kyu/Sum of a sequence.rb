e2803d98ff94c94abc5be370764a260877c676da
Maseu <Maseu@MacBook-Pro-di-mac.local>
2018-07-07 22:04:17 +0200

    yeah uploading

---
+++ 7kyu/Sum of a sequence.rb
@@ -0,0 +1,17 @@
+=begin
+
+https://www.codewars.com/kata/sum-of-a-sequence/train/ruby
+
+Your task is to make function, which returns the sum of a sequence of integers.
+
+The sequence is defined by 3 non-negative values: begin, end, step.
+
+If begin value is greater than the end, function should returns 0
+
+Examples
+
+sequenceSum(2,2,2) === 2
+sequenceSum(2,6,2) === 12 // 2 + 4 + 6
+sequenceSum(1,5,1) === 15 // 1 + 2 + 3 + 4 + 5
+sequenceSum(1,5,3) === 5 // 1 + 4
+=end
